07207c9df165a4b654a0a848ab1acb26f3330f34
junghwa <junghwa1996@naver.com>
2024-10-07 03:32:05 +0900

    input 스타일링

--- src/components/todo/TodoInput.jsx
+++ src/components/todo/TodoInput.jsx
@@ -1,14 +1,25 @@
 import { useTodoList } from "./context/TodoContext";
+import styles from "./TodoInput.module.scss";
 
 function TodoInput() {
   const { inputValue, handleInputChange, handleAddClick } = useTodoList();
   return (
-    <>
-      <input type="text" value={inputValue} onChange={handleInputChange} />
-      <button type="button" onClick={handleAddClick}>
+    <div className={styles.todoInputBox}>
+      <input
+        className={styles.todoInput}
+        type="text"
+        value={inputValue}
+        onChange={handleInputChange}
+        placeholder="오늘의 할일을 입력해주세요"
+      />
+      <button
+        className={styles.todoAddButton}
+        type="button"
+        onClick={handleAddClick}
+      >
         등록
       </button>
-    </>
+    </div>
   );
 }
 
